54c44833ccd1266b1d463205ed4fa0c97e571f9c
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-16 12:33:40 +0600

    food items component added

---
+++ src/components/FoodItems/FoodItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FoodItems = (props) => {
+    return (
+        <div>
+            <h1>This is items</h1>
+        </div>
+    );
+};
+
+export default FoodItems;
