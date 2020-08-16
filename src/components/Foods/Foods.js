4bf632090a8445dd32dacf79e6295653e73fed33
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-16 13:19:13 +0600

    loading and fd items compo added

--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -6,51 +6,64 @@ import FoodItems from "../FoodItems/FoodItems";
 const Foods = () => {
   const [foods, setFoods] = useState([]);
   const [category, setCategory] = useState("lunch");
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch("http://localhost:3010/foods")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setFoods(data);
+        setLoading(false);
       });
   }, []);
+  //filter foods according to category
   const selectCategory = foods.filter((food) => food.catagories === category);
   console.log(selectCategory);
   return (
-    <div className="container">
-      <h1>This is foods</h1>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav m-auto">
-            <li className="nav-item">
-              <span
-                className="nav-link"
-                onClick={() => setCategory("breakfast")}
-              >
-                Breakfast
-              </span>
-            </li>
-            <li className="nav-item">
-              <span className="nav-link" onClick={() => setCategory("lunch")}>
-                Lunch
-              </span>
-            </li>
+    <div>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto">
+              <li className="nav-item">
+                <span
+                  className="nav-link"
+                  onClick={() => setCategory("breakfast")}
+                >
+                  Breakfast
+                </span>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link" onClick={() => setCategory("lunch")}>
+                  Lunch
+                </span>
+              </li>
 
-            <li className="nav-item">
-              <span className="nav-link" onClick={() => setCategory("dinner")}>
-                Dinner
-              </span>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div className="row">
-        {selectCategory.map((food) => (
-          <div className="col-md-4">
-            <FoodItems foods={food}></FoodItems>
+              <li className="nav-item">
+                <span
+                  className="nav-link"
+                  onClick={() => setCategory("dinner")}
+                >
+                  Dinner
+                </span>
+              </li>
+            </ul>
           </div>
-        ))}
+        </nav>
       </div>
+      {loading ? (
+        <h3>Loading..</h3>
+      ) : (
+        <div className="container py-3">
+          <div className="row">
+            {selectCategory.map((food) => (
+              <div className="col-md-4 py-2" key={food.keys}>
+                <FoodItems foods={food}></FoodItems>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
