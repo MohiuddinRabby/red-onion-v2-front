fa51478ba6928ee3cf6468b9b2cd844b20bb6818
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-18 16:48:41 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
+import FoodItemsDetails from "./components/FoodItemsDetails/FoodItemsDetails";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 function App() {
   return (
     <>
@@ -10,6 +13,15 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home></Home>
+          </Route>
+          <Route path="/details/:keys">
+            <FoodItemsDetails></FoodItemsDetails>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/signup">
+            <Signup></Signup>
           </Route>
         </Switch>
       </Router>
