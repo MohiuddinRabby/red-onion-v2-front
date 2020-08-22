7c5a760d1142773dfb3ddcb371f24b30f00fc422
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-22 14:13:53 +0600

    cart items updated

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -1,17 +1,18 @@
 import React from "react";
-
+import "./CartItems.css";
 const CartItems = (props) => {
-  // console.log(props);
-  const { name, price, quantity } = props.items;
+  const { price, quantity, img } = props.items;
   return (
     <div>
-      <h1>Items:</h1>
-      <div className="card">
-        <div className="card-body">
-          <h3>{name}</h3>
-          <h3>{price}</h3>
-          <p>{quantity}</p>
-        </div>
+      <div className="cart-item-images">
+        <img src={img} className="img-fluid" alt="" />
+      </div>
+      <div>
+        <p>
+          <strong>
+            Price: {price} || Qty: {quantity}
+          </strong>
+        </p>
       </div>
     </div>
   );
