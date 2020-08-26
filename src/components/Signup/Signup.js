47e75bb28552c6d9f7ff4d450c32ee8c2204f8ba
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-26 19:56:00 +0600

    sign up on progress

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,41 @@
 import React from "react";
-
+import "./Signup.css";
 const Signup = () => {
   return (
-    <div className="container">
-      <h1 className="text-danger">On progress...</h1>
+    <div className="sign-page">
+      <div className="container">
+        <h3 className="text-center text-danger">Sign up</h3>
+        <form className="login-form">
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Confirm Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="btn btn-danger">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
