6bc9550123f6a4f9bdfc7ff18735b3fe0e77a2d9
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-27 20:52:55 +0600

    firebase init & signup on progress with custom auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Login.css";
-
 const Login = () => {
   return (
     <div className="login-page">
@@ -8,16 +7,15 @@ const Login = () => {
         <h3 className="text-center text-danger">Login</h3>
         <form className="login-form">
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="exampleInputEmail1">Email address</label>
             <input
               type="email"
               className="form-control"
               id="exampleInputEmail1"
-              aria-describedby="emailHelp"
             />
           </div>
           <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label htmlFor="exampleInputPassword1">Password</label>
             <input
               type="password"
               className="form-control"
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,39 +1,62 @@
 import React from "react";
 import "./Signup.css";
+import { Link } from "react-router-dom";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../../firebaseConfig";
+firebase.initializeApp(firebaseConfig);
 const Signup = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleCreateAccount = (e) => {
+    e.preventDefault();
+    console.log("auth login");
+  };
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <div className="sign-page">
       <div className="container">
         <h3 className="text-center text-danger">Sign up</h3>
         <form className="login-form">
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="exampleInputEmail1">Email address</label>
             <input
               type="email"
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
+              email="email"
             />
           </div>
           <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label htmlFor="exampleInputPassword1">Password</label>
             <input
               type="password"
               className="form-control"
               id="exampleInputPassword1"
+              name="password1"
             />
           </div>
           <div className="form-group">
-            <label for="exampleInputPassword1">Confirm Password</label>
+            <label htmlFor="exampleInputPassword2">Confirm Password</label>
             <input
               type="password"
               className="form-control"
-              id="exampleInputPassword1"
+              id="exampleInputPassword2"
+              name="password2"
             />
           </div>
-          <button type="submit" className="btn btn-danger">
+          <button
+            type="submit"
+            className="btn btn-danger"
+            onClick={handleCreateAccount}
+          >
             Submit
           </button>
+          <Link to="/login" style={{ marginLeft: "100px" }}>
+            Alreadey have an account
+          </Link>
         </form>
       </div>
     </div>
